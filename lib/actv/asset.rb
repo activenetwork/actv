e0849a00f72945ea2a517f47f063a2930bcb6d7c
ykhaburzaniya <yason.khaburzaniya@activenetwork.com>
2014-07-31 15:23:05 -0700

    adding properties for active kids and everyone else

--- lib/actv/asset.rb
+++ lib/actv/asset.rb
@@ -10,13 +10,14 @@ require 'actv/asset_topic'
 require 'actv/asset_seo_url'
 require 'actv/identity'
 require 'actv/place'
+require 'actv/recurrence'
 
 module ACTV
   class Asset < ACTV::Identity
 
     attr_reader :assetGuid, :assetName, :assetDsc, :activityStartDate, :activityStartTime, :activityEndDate, :activityEndTime,
       :homePageUrlAdr, :isRecurring, :contactName, :contactEmailAdr, :contactPhone, :showContact, :publishDate, :createdDate, :modifiedDate,
-      :authorName, :is_event, :is_article, :currencyCd, :contactTxt
+      :authorName, :is_event, :is_article, :currencyCd, :contactTxt, :regReqMinAge, :regReqMaxAge
 
     alias id assetGuid
     alias title assetName
@@ -38,6 +39,12 @@ module ACTV
     alias activity_start_date activityStartDate
     alias activity_end_date activityEndDate
     alias currency_code currencyCd
+    alias minimum_age regReqMinAge
+    alias maximum_age regReqMaxAge
+
+    def recurrence
+      @recurrence ||= ACTV::Recurrence.new(@attrs[:activityRecurrences][0]) if @attrs[:activityRecurrences].present?
+    end
 
     def place
       @place ||= ACTV::Place.new(@attrs[:place]) unless @attrs[:place].nil?
