1d0ac400df1d171db90fe19ff1b8f5f04959dd13
Jared Planter <jared.planter@activenetwork.com>
2015-02-05 16:43:25 -0600

    returns a future date if effective until date is nil

--- lib/actv/asset_price.rb
+++ lib/actv/asset_price.rb
@@ -1,9 +1,13 @@
 module ACTV
   class AssetPrice < Base
 
-    attr_reader :effectiveDate, :priceAmt, :maxPriceAmt, :minPriceAmt
+    attr_reader :priceAmt, :maxPriceAmt, :minPriceAmt
 
-    alias effective_date effectiveDate
+    def effectiveUntilDate
+      @effectiveUntilDate.nil? ? "2200-01-01T00:00:00" : @effectiveUntilDate
+    end
+
+    alias effective_date effectiveUntilDate
     alias amount priceAmt
     alias max_amount maxPriceAmt
     alias min_amount minPriceAmt
