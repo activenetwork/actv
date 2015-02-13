6676f09e7bbb2f3d1d12f1b38d2749379a4dbfdb
Marvin Min <marvin.min@activenetwork.com>
2015-02-13 15:18:20 +0800

    correct the test to make sure all the test pass

--- spec/actv/asset_price_spec.rb
+++ spec/actv/asset_price_spec.rb
@@ -2,23 +2,30 @@ require 'spec_helper'
 
 describe ACTV::AssetPrice do
 
-  before(:each) do
-    @price = ACTV::AssetPrice.new(effectiveDate: '2012-08-03T06:59:00', priceAmt: '10', maxPriceAmt: '15', minPriceAmt: '5')
+  describe 'attribute accessors and aliases' do
+    context 'when set the validate value for the asset_price' do
+      before do
+        @price = ACTV::AssetPrice.new(effectiveUntilDate: '2012-08-03T06:59:00', priceAmt: '10', maxPriceAmt: '15', minPriceAmt: '5')
+      end
+      subject { @price }
+
+      its(:effective_date){ should eq '2012-08-03T06:59:00' }
+      its(:amount){ should eq '10' }
+      its(:max_amount){ should eq '15' }
+      its(:min_amount){ should eq '5' }
+
+      its(:effectiveUntilDate){ should eq '2012-08-03T06:59:00' }
+      its(:priceAmt){ should eq '10' }
+      its(:maxPriceAmt){ should eq '15' }
+      its(:minPriceAmt){ should eq '5' }
+    end
   end
 
-  describe 'attribute accessors and aliases' do
-    subject { @price }
-
-    its(:effectiveUntilDate){ should eq '2012-08-03T06:59:00' }
-    its(:effective_date){ should eq '2012-08-03T06:59:00' }
-    its(:amount){ should eq '10' }
-    its(:max_amount){ should eq '15' }
-    its(:min_amount){ should eq '5' }
-
-    its(:effectiveDate){ should eq '2012-08-03T06:59:00' }
-    its(:priceAmt){ should eq '10' }
-    its(:maxPriceAmt){ should eq '15' }
-    its(:minPriceAmt){ should eq '5' }
+  context "when haven't effectiveUntilDate set " do
+    price = ACTV::AssetPrice.new(priceAmt: '10', maxPriceAmt: '15', minPriceAmt: '5')
+    it 'should return the default "2200-01-01T00:00:00" value for the "effectiveUntilDate"' do
+      expect(price.effectiveUntilDate).should eq '2200-01-01T00:00:00'
+    end
   end
 
 end
