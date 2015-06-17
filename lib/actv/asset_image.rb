065e9935e8016058909d23c1a86b5d07488ab530
Jared Planter <jared.planter@activenetwork.com>
2015-06-17 17:53:18 -0500

    Adds ACTV::AssetImage#url so that #url always returns a string

--- lib/actv/asset_image.rb
+++ lib/actv/asset_image.rb
@@ -1,13 +1,14 @@
 module ACTV
   class AssetImage < Base
-
     attr_reader :imageUrlAdr, :imageName, :imageCaptionTxt, :linkUrl, :linkTarget
 
-    alias url     imageUrlAdr
     alias name    imageName
     alias caption imageCaptionTxt
     alias link    linkUrl
     alias target  linkTarget
 
+    def url
+      imageUrlAdr.to_s
+    end
   end
 end
