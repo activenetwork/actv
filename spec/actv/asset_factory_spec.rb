7973b582283a6b885cb40e32690074d8a247771e
Jared Planter <jared.planter@activenetwork.com>
2015-06-18 16:33:05 -0500

    Updates specs for ACTV::AssetFactory to reflect changes using ACTV::Asset#types to determine the valid class

--- spec/actv/asset_factory_spec.rb
+++ spec/actv/asset_factory_spec.rb
@@ -3,60 +3,38 @@ require 'spec_helper'
 describe ACTV::AssetFactory do
   let(:category_name) { "" }
   let(:category_taxonomy) { "" }
-  let(:legacy_guid) { "777" }
   let(:response) { {assetGuid: "111",
-                    sourceSystem: {legacyGuid: legacy_guid},
                     assetCategories: [{category: {categoryName: category_name,
                                                   categoryTaxonomy: category_taxonomy}}]} }
   subject(:asset) { ACTV::AssetFactory.new(response).asset }
 
   describe '#asset' do
     context 'the response is not an author, event or article' do
-      it 'returns an actv asset' do
-        expect(asset).to be_a ACTV::Asset
-      end
+      it { should be_a ACTV::Asset }
     end
     context 'the response has an article category' do
       let(:category_name) { "Articles" }
-      it 'returns an actv article' do
-        expect(asset).to be_a ACTV::Article
-      end
-      it 'sets is_article? to true' do
-        expect(asset.is_article?).to be_true
-      end
+      it { should be_a ACTV::Article }
     end
-    context 'the response is from ACM' do
-      let(:legacy_guid) { "CA4EA0B1-7377-470D-B20D-BF6BEA23F040" }
-      it 'returns an actv article' do
-        expect(asset).to be_a ACTV::Article
-      end
-      it 'sets is_article? to true' do
-        expect(asset.is_article?).to be_true
-      end
+    context 'the response has an article taxonomy' do
+      let(:category_taxonomy) { "Running/Articles" }
+      it { should be_a ACTV::Article }
     end
     context 'the response has an event category' do
       let(:category_name) { "Event" }
-      it 'returns an actv event' do
-        expect(asset).to be_a ACTV::Event
-      end
-      it 'sets is_event? to true' do
-        expect(asset.is_event?).to be_true
-      end
+      it { should be_a ACTV::Event }
     end
     context 'the response has an event taxonomy' do
       let(:category_taxonomy) { "Race/Event" }
-      it 'returns an actv event' do
-        expect(asset).to be_a ACTV::Event
-      end
-      it 'sets is_event? to true' do
-        expect(asset.is_event?).to be_true
-      end
+      it { should be_a ACTV::Event }
     end
     context 'the response has an author category' do
       let(:category_name) { "Author" }
-      it 'returns an actv author' do
-        expect(asset).to be_a ACTV::Author
-      end
+      it { should be_a ACTV::Author }
+    end
+    context 'the response has an author taxonomy' do
+      let(:category_taxonomy) { "Running/Author" }
+      it { should be_a ACTV::Author }
     end
   end
 end
