788037996aebf6183f781d39394c9a702e023ee9
Jared Planter <jared.planter@activenetwork.com>
2015-06-18 16:32:23 -0500

    Refactors ACTV::AssetFactory to iterate over ACTV::Asset#types and new up the valid class type

--- lib/actv/asset_factory.rb
+++ lib/actv/asset_factory.rb
@@ -7,40 +7,11 @@ module ACTV
     end
 
     def asset
-      return ACTV::Author.new response if is_author?
-      return ACTV::Event.new response if is_event?
-      return ACTV::Article.new response if is_article?
+      ACTV::Asset.types.each do |type|
+        asset = type.new response
+        return asset if asset.valid?
+      end
       ACTV::Asset.new response
-    end
-
-    private
-
-    def is_event?
-      category_is?('event') || taxonomy_has?('event')
-    end
-
-    def is_article?
-      category_is?('articles') || acm?
-    end
-
-    def is_author?
-      category_is? 'author'
-    end
-
-    def taxonomy_has? name
-      response[:assetCategories].any? do |cat|
-        cat[:category][:categoryTaxonomy].downcase.include? name.downcase
-      end if response[:assetCategories]
-    end
-
-    def category_is? name
-      response[:assetCategories].any? do |cat|
-        cat[:category][:categoryName].downcase == name.downcase
-      end if response[:assetCategories]
-    end
-
-    def acm?
-      response[:sourceSystem][:legacyGuid].upcase == "CA4EA0B1-7377-470D-B20D-BF6BEA23F040" rescue false
     end
   end
 end
