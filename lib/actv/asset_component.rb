801601bf9e115a64e83a38c7320c45551331f47a
Jared Planter <jared.planter@activenetwork.com>
2015-07-20 16:46:35 -0500

    Adds #full_asset to AssetComponent to be called in Asset#children_assets_filtered_by_category for displaying the full child asset on a parent asset

--- lib/actv/asset_component.rb
+++ lib/actv/asset_component.rb
@@ -1,12 +1,14 @@
 module ACTV
   class AssetComponent < Base
-
     attr_reader :assetGuid
-
     alias asset_guid assetGuid
 
     def prices
       ACTV.asset(asset_guid).map(&:prices).first
+    end
+
+    def full_asset
+      ACTV.asset(asset_guid).first
     end
   end
 end
