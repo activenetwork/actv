e0532b101be7a746195ba140c3458ca9339c3dda
Blake Rodriguez <blake.rodriguez@activenetwork.com>
2015-07-22 14:24:18 -0500

    remove response header expectation

--- spec/actv/asset_component_spec.rb
+++ spec/actv/asset_component_spec.rb
@@ -9,7 +9,7 @@ describe ACTV::AssetComponent do
     before(:each) do
       stub_request(:post, "http://api.amp.active.com/v2/assets.json").
         with(:body => {"id"=>""}, :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded'}).
-        to_return(body: fixture("valid_component_asset.json"), headers: { content_type: "application/json; charset=utf-8" })
+        to_return(body: fixture("valid_component_asset.json"), headers: {})
     end
 
     it 'returns the prices associated with the component' do
