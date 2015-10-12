7fbc17f87712c96df8641c102907b7737f149fa1
Jared Planter <jaredplanter@gmail.com>
2015-10-12 16:55:56 -0500

    Uses media_type_is? in EventValidator to validate Event assets

--- lib/actv/validators/event_validator.rb
+++ lib/actv/validators/event_validator.rb
@@ -2,7 +2,7 @@ require 'actv/validators/asset_validator'
 module ACTV
   class EventValidator < AssetValidator
     def valid?
-      category_is?('event') || taxonomy_has?('event')
+      category_is?('event') || taxonomy_has?('event') || media_type_is?('event')
     end
   end
 end
