10b33dd680a0ba2ce2752e5e4cdda30965bcbac7
Enwei <303924813@qq.com>
2016-07-06 11:23:54 +0800

    Code style enhancement again

--- spec/actv/client/videos_spec.rb
+++ spec/actv/client/videos_spec.rb
@@ -9,7 +9,7 @@ describe ACTV::Client do
     context "with a valid video ID passed" do
       before do
         stub_get("/v2/assets/valid_video.json").
-        to_return(body: fixture("valid_video.json"), headers: { content_type: "application/json; charset=utf-8" })
+          to_return(body: fixture("valid_video.json"), headers: {content_type: "application/json; charset=utf-8"})
         @video = @client.video("valid_video")
       end
 
@@ -31,32 +31,32 @@ describe ACTV::Client do
     context "performs a video search with no results" do
       before do
         stub_get("/v2/search.json?query=test&category=videos").
-        to_return(body: fixture("valid_search_no_results.json"), headers: { content_type: "application/json; charset=utf-8" })
+          to_return(body: fixture("valid_search_no_results.json"), headers: {content_type: "application/json; charset=utf-8"})
       end
 
       before(:each) do
         @search_results = @client.videos('test')
       end
-      
+
       it 'returns an empty array of assets in results' do
         @search_results.results.size.should eql 0
       end
     end
-    
+
     context "performs a search with results" do
       before do
         stub_get("/v2/search.json?query=running&category=videos").
-        to_return(body: fixture("valid_search.json"), headers: { content_type: "application/json; charset=utf-8" })
+          to_return(body: fixture("valid_search.json"), headers: {content_type: "application/json; charset=utf-8"})
       end
 
       before(:each) do
         @search_results = @client.videos('running')
       end
-      
+
       it 'returns the correct array of assets in the results' do
         @search_results.results.first.assetName.should eql 'Running 5K'
         @search_results.results.size.should eql 5
       end
-    end 
+    end
   end
 end
