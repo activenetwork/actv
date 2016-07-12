bccbbd5bb5eaaffd9f7c14cbeb6436495fdde10f
Enwei <303924813@qq.com>
2016-07-12 14:55:27 +0800

    Update video search result spec

--- spec/actv/video_search_result_spec.rb
+++ spec/actv/video_search_result_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe ACTV::VideoSearchResults do
   describe '#results' do
-    it 'should return an array of ACTV::Video when results is set' do
-      results = ACTV::VideoSearchResults.new({results: [{assetGuid: '4cfb7657-6f5a-4a2a-adf3-c2b6986cb1eb', assetName: 'QA test'}, {assetGuid: 'eeb5a96a-dc6c-4a22-ad42-c0ce18832669', assetName: 'QA test2'}]}).results
-      results.should be_a Array
-      results.first.should be_a ACTV::Video
+    context "when results is set" do
+      let(:assets){ [{assetGuid: '4cfb7657-6f5a-4a2a-adf3-c2b6986cb1eb', assetName: 'QA test'}, {assetGuid: 'eeb5a96a-dc6c-4a22-ad42-c0ce18832669', assetName: 'QA test2'}] }
+      let(:results){ ACTV::VideoSearchResults.new({results: assets}).results }
+
+      it 'should return an array of ACTV::Video' do
+        expect(results).to be_a Array
+        expect(results.first).to be_a ACTV::Video
+      end
     end
 
-    it 'should return an emtpy array when results is not set' do
-      results = ACTV::VideoSearchResults.new({}).results
-      results.should eql []
+    context "when results is not set" do
+      let(:results){ ACTV::VideoSearchResults.new({}).results }
+      it "should return an empty array" do
+        expect(results).to eq([])
+      end
     end
   end
 end
