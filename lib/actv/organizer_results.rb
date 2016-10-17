2cbf104a120cf361bbba7b83efe04e5923997df6
Roger Tong <Roger.Tong@activenetwork.com>
2016-10-17 15:01:46 +0800

    ACOM-799 new properties used for pagination

--- lib/actv/organizer_results.rb
+++ lib/actv/organizer_results.rb
@@ -1,5 +1,7 @@
 module ACTV
   class OrganizerResults < ACTV::Base
+    attr_reader :item_per_page, :start_index, :total
+
     def results
       @results ||= Array(@attrs[:results]).map do |organizer|
         ACTV::Organizer.new(organizer)
@@ -8,5 +10,8 @@ module ACTV
     def total
       @total ||= @attrs[:total]
     end
+
+    alias total_results total
+    alias items_per_page item_per_page
   end
 end
