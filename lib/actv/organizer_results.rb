c244f9e843daac665a7e31674482386db2f5f97e
Roger Tong <Roger.Tong@activenetwork.com>
2016-10-21 15:10:14 +0800

    ACOM-799 change field names

--- lib/actv/organizer_results.rb
+++ lib/actv/organizer_results.rb
@@ -1,17 +1,11 @@
 module ACTV
   class OrganizerResults < ACTV::Base
-    attr_reader :item_per_page, :start_index, :total
+    attr_reader :items_per_page, :start_index, :total_results
 
     def results
       @results ||= Array(@attrs[:results]).map do |organizer|
         ACTV::Organizer.new(organizer)
       end
     end
-    def total
-      @total ||= @attrs[:total]
-    end
-
-    alias total_results total
-    alias items_per_page item_per_page
   end
 end
